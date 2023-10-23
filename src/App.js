2b39fb813821da81e2a803f0160f4cab9fe77e48
Fezan Muhammad ALi <callmetheengcountio@gmail.com>
2023-10-23 23:58:33 +0500

    commiting simple code for state using in Class Based Componenes

--- src/App.js
+++ src/App.js
@@ -7,13 +7,32 @@ class Counter extends React.Component {
     this.state = {
       count: 5,
     };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncreament = this.handleIncreament.bind(this);
   }
+
+  handleDecrement() {
+    this.setState((currtate) => {
+      return { count: currtate.count - 1 };
+    });
+  }
+
+  handleIncreament() {
+    this.setState((currtate) => {
+      return { count: currtate.count + 1 };
+    });
+  }
+
   render() {
+    const date = new Date("june 21 2027");
+    date.setDate(date.getDate() + this.state.count);
     return (
       <div>
-        <button>-</button>
-        <span>{this.state.count}</span>
-        <button>+</button>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>
+          {date.toDateString()}[{this.state.count}]
+        </span>
+        <button onClick={this.handleIncreament}>+</button>
       </div>
     );
   }
